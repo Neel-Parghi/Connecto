0e3bdc88b09d5ce17dc128e4ebb151733a1fbce0
NEEL <neelparghi192@gmail.com>
2026-01-05 22:09:40 +0530

    App Configs

--- src/web/Connecto-web/src/main.ts
+++ src/web/Connecto-web/src/main.ts
@@ -1,6 +1,24 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
+import { provideToastr } from 'ngx-toastr';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([])),
+    provideAnimations(),
+    provideToastr({
+      timeOut: 3000, 
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,   
+      closeButton: true          
+    }), provideAnimationsAsync(),
+    provideHttpClient()
+  ]
+}).catch(err => console.error(err));
